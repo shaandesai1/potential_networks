817a35a278f28e2346e5f9290c9caf594515a5a1
Shaan Desai <shaandesai@live.com>
2021-03-01 09:21:42 -0500

    new expt

--- expt_a_new.sh
+++ expt_a_new.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 #pendulum
-
-for deltat in 0.001 0.005 0.01 0.05 0.1
+for noise_var in 0 0.1
 do
-  for INTEG_STEP in 2 5 10 20
+  for deltat in 0.001 0.005 0.01 0.05 0.1
   do
-    for INTEG in rk1 rk2 rk3 rk4 vi1 vi2 vi3 vi4
+    for INTEG_STEP in 2 5 10 20
     do
-      for NONLIN in tanh softplus
+      for INTEG in rk1 rk2 rk3 rk4 vi1 vi2 vi3 vi4
       do
-        python train.py -ni 2000 -long_range 1 -nonlinearity $NONLIN -hidden_dims 250 -num_hdims 2 -lr_iters 1000 -n_test_traj 25 -n_train_traj 20 -tmax 2 -dt $deltat -srate $deltat -num_nodes 1 -dname pendulum -noise_std 0 -integrator $INTEG -fname expt_a -integ_step $INTEG_STEP
+        for NONLIN in tanh softplus
+        do
+          python train.py -ni 2000 -long_range 1 -nonlinearity $NONLIN -hidden_dims 250 -num_hdims 2 -lr_iters 1000 -n_test_traj 25 -n_train_traj 20 -tmax 2 -dt $deltat -srate $deltat -num_nodes 1 -dname pendulum -noise_std $noise_var -integrator $INTEG -fname expt_a -integ_step $INTEG_STEP
+        done
       done
     done
   done
